fab0573b4a85e61ac710cbcb939c66c51faf3d5f
leejungho <ljhmoa@gmail.com>
2023-02-19 23:19:06 +0900

    [Test] test

--- 03-jsx-in-depth/frontend/src/main.jsx
+++ 03-jsx-in-depth/frontend/src/main.jsx
@@ -1,11 +1,11 @@
-import App from './App.js';
+import App from "./App.js";
 
 // ES 구조 분해 할당
 // 번들링 (트리 쉐이킹)
 const { StrictMode } = React;
 const { createRoot } = ReactDOM;
 
-const reactDomRoot = createRoot(document.getElementById('root'));
+const reactDomRoot = createRoot(document.getElementById("root"));
 
 reactDomRoot.render(
   <StrictMode>
